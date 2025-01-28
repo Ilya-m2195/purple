620057b2dd02919fe3802508293d5366c6afbefe
Ilya-M2106 <ilya.m.9595@gmail.com>
2025-01-28 23:00:22 +0300

    create empty app, add stylelint, eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,133 @@
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      import: importPlugin,
+      prettier: prettierPlugin,
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      'import/order': [
+        'error',
+        {
+          // 'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: ['react'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+        },
+      ],
+      'import/extensions': 'off',
+      'import/prefer-default-export': 'off',
+      'consistent-return': 'off',
+      'padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: 'return',
+        },
+        {
+          blankLine: 'always',
+          prev: ['const', 'let', 'var'],
+          next: '*',
+        },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+      ],
+      'no-shadow': 'off',
+      'no-debugger': 'error',
+      'no-unused-vars': 'off',
+      'no-magic-numbers': [
+        'error',
+        {
+          ignore: [0, 1, 10, 25, 50, -1],
+        },
+      ],
+      'no-use-before-define': 'off',
+      'no-param-reassign': [
+        'error',
+        {
+          props: true,
+          ignorePropertyModificationsFor: ['state', 'self'],
+        },
+      ],
+      'default-param-last': 'off',
+      'react/display-name': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/require-default-props': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/jsx-filename-extension': [
+        1,
+        {
+          extensions: ['.jsx', '.tsx'],
+        },
+      ],
+      'react/function-component-definition': [
+        2,
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      'react/jsx-props-no-spreading': [
+        0,
+        {
+          html: 'ignore',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: true,
+          allowedNames: ['ignoredFunctionDeclaration'],
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.tsx', '.js', '.jsx'],
+          moduleDirectory: ['node_modules', 'src/'],
+        },
+      },
+      alias: {
+        map: [],
+      },
+    },
+  },
+];
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import styles from './page.module.css';
+
+const Home: FC = () => {
+  return <div className={styles.page}>Empty app</div>;
+};
+
+export default Home;
