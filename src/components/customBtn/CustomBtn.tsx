42ecba6a590ae2ed71e1ea585105212a13d11071
Ilya-M2106 <ilya.m.9595@gmail.com>
2025-02-11 21:30:10 +0300

    feat: modified CustomBtn

--- src/components/customBtn/CustomBtn.tsx
+++ src/components/customBtn/CustomBtn.tsx
@@ -1,15 +1,46 @@
-import { FC, ReactNode } from 'react';
+'use client';
+
+import { ComponentProps, FC, ReactNode, useCallback, useState } from 'react';
+import clsx from 'clsx';
 import styles from './customBtn.module.scss';
 
-interface Props {
-  count: number;
+interface Props extends ComponentProps<'button'> {
+  count?: number;
   Icon?: ReactNode;
+  viewType: 'primary' | 'circle';
 }
 
-export const CustomBtn: FC<Props> = ({ count, Icon }) => {
+export const CustomBtn: FC<Props> = ({ count, Icon, viewType, ...rest }) => {
+  const likeHandler = useCallback(async (id: number): Promise<void> => {
+    try {
+      await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: 'PATCH',
+      });
+      setIsLike(true);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        console.error(e.message);
+      }
+    }
+  }, []);
+
+  const [isLike, setIsLike] = useState(false);
+
   return (
-    <button className={styles.btn}>
-      <span className={styles.count}>{count}</span> {Icon && Icon}
+    <button
+      disabled={isLike}
+      {...rest}
+      className={clsx(
+        styles.btn,
+        viewType === 'circle' && styles.borderBtn,
+        isLike && viewType === 'circle' && styles.bgBlack,
+      )}
+      onClick={() => {
+        likeHandler(1);
+      }}
+    >
+      {count && <span className={styles.count}>{count}</span>}
+      {Icon && Icon}
     </button>
   );
 };
