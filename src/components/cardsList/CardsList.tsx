614068fb6d8bad8c2979ec20832c5c6ae6ca9773
Ilya-M2106 <ilya.m.9595@gmail.com>
2025-02-18 21:34:42 +0300

    feat: geted posts-data from jsonplhldr, render posts on server

--- src/components/cardsList/CardsList.tsx
+++ src/components/cardsList/CardsList.tsx
@@ -2,18 +2,37 @@ import { FC } from 'react';
 import styles from './cardsList.module.scss';
 import { Card } from '@/components';
 
-export const CardsList: FC = () => {
+interface IPost {
+  userId: string;
+  id: string;
+  title: string;
+  body: string;
+}
+
+const getPostsData = async (): Promise<Array<IPost> | null> => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+
+  if (!data.ok) {
+    return null;
+  }
+
+  return data.json();
+};
+
+export const CardsList: FC = async () => {
+  const data = await getPostsData();
+
+  if (!data) {
+    return <div>Посты не найдены</div>;
+  }
+
   return (
     <ul className={styles.cardsList}>
-      <li className={styles.cardsItem}>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
+      {data.map(post => (
+        <li className={styles.cardsItem} key={post.id}>
+          <Card />
+        </li>
+      ))}
     </ul>
   );
 };
